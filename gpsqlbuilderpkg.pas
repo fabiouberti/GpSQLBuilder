40fcecc606e0f3869db6cbd32ff4eeeccc99b0ef
Fábio Luiz Uberti <fabiosystema@gmail.com>
2019-06-04 20:20:57 -0300

    Feitas implementações para compilar como package para delphi

--- gpsqlbuilderpkg.pas
+++ gpsqlbuilderpkg.pas
@@ -1,14 +1,12 @@
-{ This file was automatically created by Lazarus. Do not edit!
-  This source is only used to compile and install the package.
- }
-
 unit GpSQLBuilderPkg;
 
 interface
 
 uses
-  GpSQLBuilder.AST, GpSQLBuilder, GpSQLBuilder.Serialize, 
-  System.Generics.Collections, LazarusPackageIntf;
+  GpSQLBuilder.AST,
+  GpSQLBuilder,
+  GpSQLBuilder.Serialize,
+  System.Generics.Collections;
 
 implementation
 
@@ -17,5 +15,5 @@ begin
 end;
 
 initialization
-  RegisterPackage('GpSQLBuilderPkg', @Register);
+
 end.
